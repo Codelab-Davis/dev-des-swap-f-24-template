ca68d2edf9660611743a16e10ead5b5c4290897c
rasooly-dev <alofiabdulprogramming@gmail.com>
2024-11-11 00:35:19 -0800

    feat: enhance database initialization with file existence check and logging

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,9 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 
-export const db = drizzle(new Database('../dev-des-swap.db'));
+export const db = drizzle(
+    new Database('dev-des-swap.db', {
+        fileMustExist: true,
+        verbose: console.log,
+    })
+);
